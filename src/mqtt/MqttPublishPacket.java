2b2d0aafdaa8066ea13187def65108b0d6ce8f9c
BlakeCode <472369577@qq.com>
2020-10-17 19:55:53 +0800

    MqttPublishpacket

--- src/mqtt/MqttPublishPacket.java
+++ src/mqtt/MqttPublishPacket.java
@@ -1,4 +1,22 @@
 package mqtt;
 
-public class MqttPublishPacket {
+/**
+ * author blake
+ * date 2020-10-17 16:00:43 
+ **/
+
+public class MqttPublishPacket extends MqttPacket {
+
+    public MqttPublishPacket(MqttFixedHeader mqttFixedHeader, MqttPublishVariableHeader variableHeader, byte[] payload) {
+        super(mqttFixedHeader, variableHeader, payload);
+    }
+
+    public MqttFixedHeader getPublishFixedHeader() { return super.getMqttFixedHeader(); }
+
+    public MqttPublishVariableHeader getPublishVariableHeader() { return (MqttPublishVariableHeader)super.getVariableHeader(); }
+
+    public byte[] getPublishPayload() { return (byte[])super.payload; }
+
+    // public String getPublishMessage() throws Exception { return MqttUtil.decodeUTF8StringToString(this.getPublishPayload()); }
+
 }
