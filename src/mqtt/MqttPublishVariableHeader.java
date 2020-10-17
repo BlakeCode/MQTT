f3e11aa153a4b2d7791a8cc0561cf253bb82cdd1
BlakeCode <472369577@qq.com>
2020-10-17 19:57:46 +0800

    MqttPublishVariableHeader

---
+++ src/mqtt/MqttPublishVariableHeader.java
@@ -0,0 +1,38 @@
+package mqtt;
+
+/**
+ * author blake
+ * date 2020-10-17 16:00:48
+ **/
+
+public class MqttPublishVariableHeader {
+
+    private String topicName;
+    private int packetIdentifier;
+    private MqttProperties publishProperties;
+
+    public MqttPublishVariableHeader(String topicName, int packetIdentifier) {
+        this(topicName, packetIdentifier, null);
+    }
+
+    public MqttPublishVariableHeader(String topicName, int packetIdentifier, MqttProperties publishProperties) {
+        this.topicName = topicName;
+        this.packetIdentifier = packetIdentifier;
+        this.publishProperties = publishProperties;
+    }
+
+    public String getTopicName() { return topicName; }
+
+    public int getPacketIdentifier() { return packetIdentifier; }
+
+    public MqttProperties getPublishProperties() { return publishProperties; }
+
+    @Override
+    public String toString() {
+        return "MqttPublishVariableHeader{" +
+                "topicName='" + topicName + '\'' +
+                ", packetIdentifier=" + packetIdentifier +
+                '}';
+    }
+
+}
